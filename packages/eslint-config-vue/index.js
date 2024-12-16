004545f8b7990059c5ce967acddaf7d6d34baa8f
lfzs <jycomputer_417@163.com>
2024-12-16 09:41:38 +0800

    feat: close rule

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -10,6 +10,7 @@ export default [
       'vue/multi-word-component-names': 0,
       'vue/no-watch-after-await': 0,
       'vue/attribute-hyphenation': [1, 'never'],
+      'vue/html-self-closing': 0,
       'vue/max-attributes-per-line': 0, // 和 prettier printWidth 冲突
       'vue/prop-name-casing': 0,
       'vue/singleline-html-element-content-newline': 0,
